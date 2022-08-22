d198be372fa29519df304999eada3856484f49da
Progra190 <fndani26@gmail.com>
2022-08-21 22:51:21 -0300

    Treino1
    Feito no domingo dia 21082022

---
+++ inicio.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE-edge">
+<title>Treino 1</title>
+<link rel="stylesheet" href="">
+    </head>
+<body>
+    <h1>
+    <?php
+    echo "Olá mundo";
+    ?></h1>
+</body>
+</html>
